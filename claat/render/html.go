a9849d08bc744009000d953cf491bfe3bdcd363d
katopz <katopz@gmail.com>
2021-01-13 14:51:01 +0700

    Try iframe loading="lazy"

--- claat/render/html.go
+++ claat/render/html.go
@@ -378,7 +378,7 @@ func (hw *htmlWriter) youtube(n *types.YouTubeNode) {
 }
 
 func (hw *htmlWriter) iframe(n *types.IframeNode) {
-	hw.writeFmt(`<iframe class="embedded-iframe" src=%q allow="clipboard-write" allowfullscreen
+	hw.writeFmt(`<iframe class="embedded-iframe" src=%q allow="clipboard-write" allowfullscreen loading="lazy"
 	onload="iframe_onload(this)"></iframe>`, n.URL)
 }
 
